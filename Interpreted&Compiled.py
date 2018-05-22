c62818d8a09f21e28b9fd98c061d39260213a594
Yourong Ye <34338026+YourongYe@users.noreply.github.com>
2018-05-22 14:50:37 +0800

    Create Interpreted&Compiled.py

---
+++ Interpreted&Compiled.py
@@ -0,0 +1,6 @@
+Interpreted Programming Language:
+The interpreter executes the program directly, translating each statement into a sequence of one or more subroutines, 
+and then into another language (often machine code).
+
+Compiled Programming Language:
+Compilers (translators that generate machine code from source code),compiling a program into machine-language instructions.
