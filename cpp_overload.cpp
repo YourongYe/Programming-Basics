cba09111460603f5fd1abea775aaacd5673c7026
Yourong Ye <34338026+YourongYe@users.noreply.github.com>
2018-06-03 17:01:01 +0100

    Create cpp_overload.cpp

---
+++ cpp_overload.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int add(int a, int b){
+    return a+b;
+}
+
+int add(int a, int b, int c){
+    return a+b+c;
+}
+
+std::string add(std::string a, std::string b, std::string c){
+    return a+b+c;
+}
+
+int main()
+{
+    cout<<add(1,2)<<endl;
+    cout<<add(1,2,3)<<endl;
+    cout<<add("a","b","blabla")<<endl;
+
+    return 0;
+}
